db702660d191955b28b72aa599fd5c5a3c5acb5e
asikckcm <asikckcm@outlook.com>
2025-04-01 13:19:11 +0800

    UPDATED!

--- script/april.js
+++ script/april.js
@@ -1,5 +1,6 @@
 function bomb() {
-    document.getElementById("message").innerHTML = "都说了叫你别点";
+    document.getElementById("message").innerHTML = "都说了叫你别点<br/>你掉进声波陷阱了！<br/>\
+    让你按按钮是为了绕开浏览器直接播放音频的限制";
     const music = new Audio("bomb.mp3")
     music.play()
 }
